d452911f5110da3ef2a2ca0ed890df5ef75ad3ca
Moti <gansky.m@gmail.com>
2020-05-28 17:49:54 +0300

    sd

---
+++ basics/jenkins/job-dsl/nodejs.groovy
@@ -0,0 +1,45 @@
+job('NodeJS example') {
+    scm {
+        git('git://github.com/yanivomc/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
+
+job('NodeJS Docker example') {
+    scm {
+        git('git://github.com/yanivomc/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') 
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('yanivomc/docker-nodejs-demo') //qa / dev
+            buildContext('./basics')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
